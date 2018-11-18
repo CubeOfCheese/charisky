b225798f59d2015d121198a72a7dcba9394d85dc
jhvoong <jhvoong@ad3.ucdavis.edu@pc40.cs.ucdavis.edu>
2018-11-18 01:27:40 -0800

    Made timer buttons disappear on click

---
+++ home/script.js
@@ -0,0 +1,46 @@
+function startTimer(duration, display) {
+  console.log("started Timer")
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (!document.hasFocus())
+          document.location.href = "#"; // need to write
+
+        if (--timer < 0) {
+            timer = 0; //win
+
+        }
+        // if (timer == 0) {
+        //     break;
+        // }
+    }, 1000);
+}
+
+
+function startClick() {
+  var seconds = parseInt(document.getElementById("secondsIn").value);
+  var minutes = parseInt(document.getElementById("minutesIn").value);
+
+  document.getElementsByClassName("in1")[0].style.visibility = "hidden";
+  document.getElementsByClassName("in1")[1].style.visibility = "hidden";
+  document.getElementsByClassName("in1")[2].style.visibility = "hidden";
+
+  console.log(seconds);
+  console.log(minutes*60);
+  var setTime = seconds + (minutes*60);
+  console.log(setTime);
+  var display = document.querySelector('#time');
+  startTimer(setTime, display);
+}
+
+
+function fail() {
+  
+}
