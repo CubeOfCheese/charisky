b4c7a803ea35249c25e7d311497c7b182f6ad8c5
CubeOfCheese <cubeofcheese@gmail.com>
2018-11-17 19:32:10 -0800

    bad attempts

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function startTimer(duration, display) {
   console.log("started Timer")
     var timer = duration, minutes, seconds;
@@ -7,7 +8,13 @@ function startTimer(duration, display) {
 
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
-
+        // if (window.name != "Charisky") {
+        //   console.log(window.name);
+        //   console.log("new tab opened");
+        // }
+        if (window.history.length != tabsOpen) {  // Um, needs to be 0 for IE, 1 for Firefox
+            console.log("new tab opened")
+        }
         display.textContent = minutes + ":" + seconds;
         if (--timer < 0) {
             timer = 0;
@@ -19,7 +26,12 @@ function startTimer(duration, display) {
 }
 
 
+
 function startClick() {
+  window.name = "Charisky";
+  console.log(window.name);
+  console.log(window.history.length);
+  var tabsOpen = window.history.length;
   var seconds = parseInt(document.getElementById("secondsIn").value);
   var minutes = parseInt(document.getElementById("minutesIn").value);
   console.log(seconds);
